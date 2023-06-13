1a4aa58043adc6ce319f0041614dfe3144aaf0c7
Denys Vasyliev <den.vasyliev@gmail.com>
2023-06-13 12:40:55 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,15 @@
-output "kubeconfig" {
-  value       = "${path.module}/kind-config"
-  description = "The path to the kubeconfig file"
+output "client_key" {
+  value = kind_cluster.this.client_key
+}
+
+output "ca" {
+  value = kind_cluster.this.cluster_ca_certificate
+}
+
+output "crt" {
+  value = kind_cluster.this.client_certificate
+}
+
+output "endpoint" {
+  value = kind_cluster.this.endpoint
 }
